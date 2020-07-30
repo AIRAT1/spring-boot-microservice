aa6ba2acb2193793ea1260bf2b9c04e19a7bcd47
Ayrat <ayrat1@mail.ru>
2020-07-31 00:56:43 +0300

    15.00

--- src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -1,13 +1,63 @@
 package com.example.reservationservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ReservationServiceApplication.class, args);
     }
+}
 
+@Component
+class DummyCLR implements CommandLineRunner {
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public DummyCLR(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Stream.of("Spencer", "Mark", "Stefan", "Maria", "Arnaud", "Antonio")
+                .forEach(n -> reservationRepository.save(new Reservation(n)));
+        reservationRepository.findAll().forEach(System.out::println);
+    }
+}
+
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+}
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+class Reservation {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String reservationName;
+
+    public Reservation(String reservationName) {
+        this.reservationName = reservationName;
+    }
 }
